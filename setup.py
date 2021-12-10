25ebc8f5f57a0144e48281acd0e2244ac86fc943
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2021-12-10 16:28:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'easygans',         # How you named your package folder (MyLib)
   packages = ['easygans'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple keras library to train and serve GANs',   # Give a short description about your library
   author = 'Vishnu N',                   # Type in your name
   author_email = 'vishnunkumar25@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Vishnunkumar/easygans',   # Provide either the link to your github or to your website
-  download_url ='https://github.com/Vishnunkumar/easygans/archive/refs/tags/v_03.tar.gz',    # I explain this later on
+  download_url ='https://github.com/Vishnunkumar/easygans/archive/refs/tags/v_04.tar.gz',    # I explain this later on
   keywords = ['Computer Vision', 'Machine learning', 'GANS', 'TensorFlow', 'Keras'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'scipy==1.1.0',
